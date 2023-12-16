0fd4ba7fbfd531f4c207891f10a8bde703549181
Vladislav <novozheninv@bk.ru>
2023-12-16 12:08:21 +0300

    build2

--- public/client/client.js
+++ public/client/client.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1a119530641ed24cb694";
+/******/ 	var hotCurrentHash = "2b4f27516d50e427ac91";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
